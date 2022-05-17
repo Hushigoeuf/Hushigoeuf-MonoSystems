6c18f82c935ef0ef352a39dee2b24f499c9bfbe4
Hushigoeuf <93216552+Hushigoeuf@users.noreply.github.com>
2022-05-18 00:14:40 +0300

    changed

---
+++ Scripts/HGBaseExtension.cs
@@ -0,0 +1,80 @@
+﻿namespace Hushigoeuf
+{
+    public enum HGExtensionPositions
+    {
+        Self,
+        Parent,
+        Hierarchy,
+        Custom
+    }
+
+    public abstract class HGBaseExtension : HGMonoBehaviour
+    {
+        /// Где находится расширение по отношению к родителю (по умолчанию как компонент этого же объекта)
+        public virtual HGExtensionPositions ExtensionPosition => HGExtensionPositions.Self;
+
+        /// ID расширения по которому с ним можно работать в дальнейшем
+        public virtual string ExtensionID => null;
+
+        protected bool _initialized;
+
+        public bool Initialized => _initialized;
+
+        protected virtual void Awake()
+        {
+        }
+
+        protected virtual void Start()
+        {
+            Initialization();
+        }
+
+        protected virtual void OnEnable()
+        {
+        }
+
+        protected virtual void OnDisable()
+        {
+        }
+
+        protected virtual void OnDestroy()
+        {
+        }
+
+        /// <summary>
+        /// Инициализирует расширение.
+        /// Инициализация происходит по порядку, от родителя вниз по лестнице.
+        /// </summary>
+        public virtual void Initialization()
+        {
+            if (!_initialized)
+            {
+                _initialized = true;
+                OnInitialization();
+            }
+        }
+
+        protected virtual void OnInitialization()
+        {
+        }
+
+        /// <summary>
+        /// Регистрирует новое дочерние расширение.
+        /// </summary>
+        public virtual void RegisterSecondExtension(HGBaseExtension target)
+        {
+        }
+
+        /// <summary>
+        /// Проверяет, является ли заданная цель родителем.
+        /// </summary>
+        public virtual bool CompareFirstExtension(HGBaseExtension target) => false;
+
+        /// <summary>
+        /// Кастомный сброс расширения.
+        /// </summary>
+        public virtual void ResetExtension(bool value)
+        {
+        }
+    }
+}
---
+++ Scripts/HGSingletonExtension.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Hushigoeuf
+{
+    /// <summary>
+    /// Этот класс определяет компонент как расширение-одиночку.
+    /// Такое расширение может быть только первичным и существовать в одном экземпляре.
+    /// </summary>
+    public abstract class HGSingletonExtension<TFirstExtension, TSecondExtension> : HGFirstExtension<TSecondExtension>
+        where TFirstExtension : HGBaseExtension where TSecondExtension : HGBaseExtension
+    {
+        protected static TFirstExtension _instance;
+
+        public static TFirstExtension Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = FindObjectOfType<TFirstExtension>();
+                return _instance;
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (!Application.isPlaying) return;
+
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this as TFirstExtension;
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            if (_instance == this)
+                _instance = null;
+        }
+    }
+
+    /// <summary>
+    /// Можно переопределить этот класс, чтобы не задавать тип дочернего расширения.
+    /// </summary>
+    public abstract class HGSingletonExtension<TFirstExtension> :
+        HGSingletonExtension<TFirstExtension, HGFirstExtension<HGBaseExtension>>
+        where TFirstExtension : HGBaseExtension
+    {
+    }
+}
